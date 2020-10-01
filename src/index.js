835b5475645f4daad91074ed20a889d5f2d4db60
Mephis28 <sergei.krn@gmail.com>
2020-10-01 20:36:01 +0300

    init: finished typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array) {
+        array = array.sort((a, b) => {
+            return a - b
+        });
+        return (array[0] = array[0] === undefined ? 0 : array[0]);
+    }
+    return 0
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array) {
+        array = array.sort((a, b) => {
+            return a - b
+        });
+        return (array[array.length - 1] = array[array.length - 1] === undefined ? 0 : array[array.length - 1]);
+    }
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array) {
+        let arrayLength = array.length > 0 ? array.length : 1;
+        return array.reduce((x, y) => x + y, 0) / arrayLength;
+    }
+    return 0
 }
